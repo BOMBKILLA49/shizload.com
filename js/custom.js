4d470619bfc923b0c88ad82849b40560687b5351
BOMBKILLA49 <129550633+BOMBKILLA49@users.noreply.github.com>
2025-04-02 22:56:22 -0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,193 +1,165 @@
-/*-----------------------------------------------------------------------------------
+/*========================================================================
+EXCLUSIVE ON themeforest.net
+Template Name   : DOMINA - Domain sale and auction landing page
+Author: ThemeAtelier
+Created: March 2019
+Last update: March 2019
+========================================================================*/
+/* TABLE OF CONTENTS
+	1. PRELOADER
+	2. PRACTICLEGROUND
+	3. CONTACT FORM
+	4. AJAX MAILCHIMP SUBSCRIBE
+	5. LOCAL SUBSCRIPTION FORM
+*/
+var PATH = {};
+(function ($) {
+  "use strict";
 
-    Template Name: Websole
+  /******************** 1. PRELOADER ********************/
+  PATH.preLoader = function () {
+    $(".loader").fadeOut();
+    $(".loader-wrapper").delay(1000).fadeOut("slow");
+  };
+  /******************** 2. PRACTICLEGROUND ********************/
+  PATH.practicalGround = function () {
+    var particles = document.getElementById("particles");
+    if (particles) {
+      particleground(particles, {
+        dotColor: "#e6ebf3",
+        lineColor: "#e6ebf3",
+      });
+    }
+  };
 
-
-    Note: This is Custom Js file
-  
------------------------------------------------------------------------------------*/
- 
- $( document ).ready(function() {
-      var new_scroll_position = 0; 
-        var last_scroll_position; 
-        var header = document.getElementById("stickyHeader");
-        window.addEventListener('scroll', function(e) {
-        last_scroll_position = window.scrollY;
-        if (new_scroll_position < last_scroll_position && last_scroll_position > 100) {
-           header.classList.remove("slideDown"); 
-          header.classList.add("slideUp"); 
-        }  
-        else if (last_scroll_position < 100) { 
-          header.classList.remove("slideDown"); 
-        }  
-        else if (new_scroll_position > last_scroll_position) { 
-          header.classList.remove("slideUp"); 
-          header.classList.add("slideDown"); 
+  /******************** 3. CONTACT FORM ********************/
+  PATH.contactForm = function () {
+    function isValidEmail(emailAddress) {
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      return pattern.test(emailAddress);
+    }
+    var $contact = $("#contact-form, #offer-form");
+    if ($contact.length) {
+      $contact.on("submit", function (e) {
+        e.preventDefault();
+        var success = $(this).find(".email-success"),
+          failed = $(this).find(".email-failed"),
+          loader = $(this).find(".email-loading"),
+          postUrl = $(this).attr("action");
+        var data = {
+          name: $(this).find(".contact-name").val(),
+          email: $(this).find(".contact-email").val(),
+          subject: $(this).find(".contact-subject").val(),
+          message: $(this).find(".contact-message").val(),
+        };
+        if (
+          isValidEmail(data["email"]) &&
+          data["message"].length > 1 &&
+          data["name"].length > 1
+        ) {
+          $.ajax({
+            type: "POST",
+            url: postUrl,
+            data: data,
+            beforeSend: function () {
+              loader.fadeIn(1000);
+            },
+            success: function (data) {
+              loader.fadeOut(1000);
+              success.delay(500).fadeIn(1000);
+              failed.fadeOut(500);
+            },
+            error: function (xhr) {
+              // if error occured
+              loader.fadeOut(1000);
+              failed.delay(500).fadeIn(1000);
+              success.fadeOut(500);
+            },
+            complete: function () {
+              loader.fadeOut(1000);
+            },
+          });
+        } else {
+          loader.fadeOut(1000);
+          failed.delay(500).fadeIn(1000);
+          success.fadeOut(500);
         }
- 
-          new_scroll_position = last_scroll_position;
-
-        }); 
+        return false;
       });
-/*************  accordion-item ****************/
+    }
+  };
 
-$('.accordion-item .heading').on('click', function(e) {
+  /******************** 4. AJAX MAILCHIMP SUBSCRIBE ********************/
+  $("#subscribe-mailchimp").ajaxChimp({
+    callback: mailchimpCallback,
+    url: "https://site11.us3.list-manage.com/subscribe/post?u=post&amp;id=ID", // Replace your mailchimp post url inside double quote "".
+  });
+
+  function mailchimpCallback(resp) {
+    var error_msg = $("#subscribe-mailchimp").find(".error-msg");
+    var success_msg = $("#subscribe-mailchimp").find(".success-msg");
+
+    if (resp.result === "success") {
+      error_msg.fadeOut(200);
+      success_msg.html(resp.msg).fadeIn(200);
+    } else if (resp.result === "error") {
+      success_msg.fadeOut(200);
+      error_msg.html(resp.msg).fadeIn(200);
+    }
+  }
+
+  /******************** 5. LOCAL SUBSCRIPTION FORM ********************/
+  $("#subscribe").submit(function (e) {
     e.preventDefault();
+    var email = $("#subscriber-email").val();
+    var dataString = "email=" + email;
 
-    if($(this).closest('.accordion-item').hasClass('active')) {
-        $('.accordion-item').removeClass('active');
-    } else {
-        $('.accordion-item').removeClass('active');
-
-        $(this).closest('.accordion-item').addClass('active');
+    function isValidEmail(emailAddress) {
+      var pattern = new RegExp(
+        /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i
+      );
+      return pattern.test(emailAddress);
     }
-    var $content = $(this).next();
-    $content.slideToggle(100);
-    $('.accordion-item .content').not($content).slideUp('fast');
-});
- 
- 
-function inVisible(element) { 
-  var WindowTop = $(window).scrollTop();
-  var WindowBottom = WindowTop + $(window).height();
-  var ElementTop = element.offset().top;
-  var ElementBottom = ElementTop + element.height(); 
-  if ((ElementBottom <= WindowBottom) && ElementTop >= WindowTop)
-    animate(element);
-}
 
-function animate(element) { 
-  if (!element.hasClass('ms-animated')) {
-    var maxval = element.data('max');
-    var html = element.html();
-    element.addClass("ms-animated");
-    $({
-      countNum: element.html()
-    }).animate({
-      countNum: maxval
-    }, { 
-      duration: 5000,
-      easing: 'linear',
-      step: function() {
-        element.html(Math.floor(this.countNum) + html);
-      },
-      complete: function() {
-        element.html(this.countNum + html);
-      }
-    });
-  }
-
-}
- 
-$(function() { 
-  $(window).scroll(function() {
-     $("h2[data-max]").each(function() {
-      inVisible($(this));
-    });
-  })
-}); 
-
-  
-/* 6. loaded */  
-$(window).on('load', function () {
-    $("body").addClass("page-loaded"); 
-}); 
-
-
-/* heading hover  */
-
-
-(function($) {
-    function title_animation() {
-    var tg_var = jQuery('.sec-title-animation');
-    if (!tg_var.length) {
-      return;
-    }
-    const quotes = document.querySelectorAll(".sec-title-animation .title-animation");
-
-    quotes.forEach(quote => {
-
-      //Reset if needed
-      if (quote.animation) {
-        quote.animation.progress(1).kill();
-        quote.split.revert();
-      }
-
-      var getclass = quote.closest('.sec-title-animation').className;
-      var animation = getclass.split('animation-');
-      if (animation[1] == "style4") return
-
-      quote.split = new SplitText(quote, {
-        type: "lines,words,chars",
-        linesClass: "split-line"
-      });
-      gsap.set(quote, {
-        perspective: 400
-      });
- 
-      if (animation[1] == "style2") {
-        gsap.set(quote.split.chars, {
-          opacity: 0,
-          x: "50"
-        });
-      } 
-      quote.animation = gsap.to(quote.split.chars, {
-        scrollTrigger: {
-          trigger: quote,
-          start: "top 90%",
+    if (isValidEmail(email)) {
+      $.ajax({
+        type: "POST",
+        url: "subscribe/subscribe.php",
+        data: dataString,
+        success: function () {
+          $(".success-msg").fadeIn(1000);
+          $(".error-msg").fadeOut(500);
+          $(".hide-after").fadeOut(500);
         },
-        x: "0",
-        y: "0",
-        rotateX: "0",
-        opacity: 1,
-        duration: 1,
-        ease: Back.easeOut,
-        stagger: .02
       });
-    });
-  }
-  ScrollTrigger.addEventListener("refresh", title_animation);
-  
-    
-    $(window).on('load', function() {
-         title_animation();
-    });
- 
-})(window.jQuery);
-
-
-
-function scrollTopPercentage() {
-  const scrollPercentage = () => {
-    const scrollTopPos = document.documentElement.scrollTop;
-    const calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrollValue = Math.round((scrollTopPos / calcHeight) * 100);
-    const scrollElementWrap = $("#scroll-percentage");
-
-    scrollElementWrap.css("background", `conic-gradient( #fff ${scrollValue}%, #000 ${scrollValue}%)`);
-
-    // ScrollProgress
-    if (scrollTopPos > 100) {
-      scrollElementWrap.addClass("active");
     } else {
-      scrollElementWrap.removeClass("active");
+      $(".error-msg").fadeIn(1000);
     }
 
-    if (scrollValue < 99) {
-      $("#scroll-percentage-value").text(`${scrollValue}%`);
-    } else {
-      $("#scroll-percentage-value").html('<i class="fa-solid fa-arrow-up-long"></i>');
-    }
+    return false;
+  });
+
+  /******************** 6. CURRENT YEAR ********************/
+  var yearElement = document.getElementById("year");
+  if (yearElement !== null) {
+	yearElement.innerHTML = new Date().getFullYear();
   }
-  window.onscroll = scrollPercentage;
-  window.onload = scrollPercentage;
-  // Back to Top
-  function scrollToTop() {
-    document.documentElement.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-  $("#scroll-percentage").on("click", scrollToTop);
-}
-scrollTopPercentage(); 
+  /******************** 2.15 WOW JS ********************/
+  var wow = new WOW({
+    offset: 50,
+    mobile: true,
+  });
+  wow.init();
+
+  /* Document ready function */
+  $(function () {
+    PATH.practicalGround();
+    PATH.contactForm();
+  });
+  /* Window on load function */
+  $(window).on("load", function () {
+    PATH.preLoader();
+  });
+})(jQuery);
